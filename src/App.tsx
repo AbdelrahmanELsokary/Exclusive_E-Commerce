db5c81e1e6899f66fcaa0f19bcf5d88c4afbc313
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-04-06 23:03:31 +0200

    Setup: Translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
+import { BrowserRouter } from 'react-router';
+import { Routes } from 'react-router';
+
 import { Header } from './components/layout/Header';
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
       <Header />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-// import App from './App.tsx';
-import { BrowserRouter } from 'react-router';
-import { Header } from './components/layout/Header.tsx';
-import { Routes } from 'react-router';
+import '../public/assets/styles/index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Header />
-      {/* <App /> */}
-      <Routes></Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
