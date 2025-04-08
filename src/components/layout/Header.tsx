69703cd232c35ee58e709084214bbeea7797aee3
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-04-09 01:42:58 +0200

    Add: Drop Down To Account Of Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,15 @@
 import { IoCartOutline, IoSearch } from 'react-icons/io5';
+import { IoIosCloseCircleOutline } from 'react-icons/io';
+import { LuShoppingBag } from 'react-icons/lu';
 import { useTranslation } from 'react-i18next';
 import { Link, NavLink } from 'react-router';
+import { FaRegStar } from 'react-icons/fa';
+import { TbLogout2 } from 'react-icons/tb';
 import { CiHeart } from 'react-icons/ci';
 import { FiUser } from 'react-icons/fi';
 import { MdMenu } from 'react-icons/md';
-
 import { useState } from 'react';
+
 export function Header() {
   const { t } = useTranslation();
   const navLinks: { name: string; path: string }[] = [
@@ -15,10 +19,10 @@ export function Header() {
     { name: t('Sign Up'), path: '/SignUp' },
   ];
   const [isMenuOpen, setisMenuOpen] = useState(false);
-  // const [account, setAccount] = useState(false);
+  const [account, setAccount] = useState(false);
   return (
     <>
-      <header className=" sticky top-0 bg-inherit border-b border-[#00000036] z-10 py-5">
+      <header className="sticky top-0 border-b border-[#00000036] z-10 py-5">
         <div className="container justify-around flex items-center gap-4 m-auto">
           <Link className="font-bold text-2xl tracking-[1px]" to={'/'}>
             {t('Exclusive')}
@@ -45,11 +49,43 @@ export function Header() {
             <div className="cart">
               <IoCartOutline className="text-2xl cursor-pointer" />
             </div>
-            <FiUser className="w-[32px] h-[32px] text-white bg-[#DB4444] rounded-[50%] p-[6px] cursor-pointer" />
+            <FiUser onClick={() => setAccount(!account)} className="w-[32px] h-[32px] text-white bg-[#DB4444] rounded-[50%] p-[6px] cursor-pointer" />
           </div>
           <button className="lg:hidden" onClick={() => setisMenuOpen(!isMenuOpen)} type="button">
             <MdMenu className="text-3xl cursor-pointer" />
           </button>
+          <ul className={`${account ? 'text-white absolute right-0 top-23 max-w-[300px] rounded-[4px] bg-[#00000014] backdrop-blur-md px-4' : 'hidden'}`}>
+            <li>
+              <Link className="flex items-center gap-2 py-3" to={'/'}>
+                <FiUser className="text-2xl" />
+                Manage My Account
+              </Link>
+            </li>
+            <li>
+              <Link className="flex items-center gap-2 py-3" to={'/'}>
+                <LuShoppingBag className="text-2xl" />
+                My Order
+              </Link>
+            </li>
+            <li>
+              <Link className="flex items-center gap-2 py-3" to={'/'}>
+                <IoIosCloseCircleOutline className="text-2xl" />
+                My Cancellations
+              </Link>
+            </li>
+            <li>
+              <Link className="flex items-center gap-2 py-3" to={'/'}>
+                <FaRegStar className="text-2xl" />
+                My Reviews
+              </Link>
+            </li>
+            <li>
+              <button type="button" className="flex items-center gap-2 py-3">
+                <TbLogout2 className="text-2xl" />
+                Logout
+              </button>
+            </li>
+          </ul>
         </div>
       </header>
     </>
