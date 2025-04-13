fa8cdeed05e765766e0cdb7ccb80f031ef176019
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-04-13 14:40:06 +0200

    Add: Sign Up Component Without Logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import '../public/assets/styles/App.css';
 import { BrowserRouter, Route } from 'react-router';
 import { Routes } from 'react-router';
 import { PageLayout } from './components/layout/PageLayout';
+import { SignUp } from './components/auth/Signup';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
             <Route element={<p>home</p>} path="/" />
             <Route element={<p>Contact</p>} path="/Contact" />
             <Route element={<p>Contact</p>} path="/About" />
-            <Route element={<p>Contact</p>} path="/SignUp" />
+            <Route element={<SignUp />} path="/SignUp" />
           </Route>
         </Routes>
       </BrowserRouter>
